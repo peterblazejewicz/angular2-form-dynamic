65782b2bd3bea9dd5bef6ea27aa87d0655272bd1
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-03-02 23:12:49 +0100

    Quick app bootstrapping

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Hello world! This is HTML5 Boilerplate.</h1>'
+})
+
+export class AppComponent {
+  constructor() { }
+}
